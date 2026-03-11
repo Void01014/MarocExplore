8fbce29c3ba5cca83a6ddc27e3db1457e0e88b93
Omar EL FILALI <omarelfiie2007@gmail.com>
2026-03-11 15:45:08 +0000

    Implement create method in ItineraryController with validation and itinerary creation logic

--- app/Http/Controllers/ItineraryController.php
+++ app/Http/Controllers/ItineraryController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Itinerary;
 use Illuminate\Http\Request;
 
 class ItineraryController extends Controller
 {
-    
+    public function create(Request $request)
+    {
+        $rules = [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
+        ];
+
+        $validated = $request->validate($rules);
+
+        $itenerary = Itinerary::create([
+            'user_id' => '' ?? '',
+            'name' => '' ?? '',
+            'title' => '' ?? '',
+            'duration' => '' ?? '',
+            'image' => '' ?? '',
+            'category_id' => '' ?? ''
+        ]);
+
+        foreach ($validated['destinations'] as $destination) {
+            $destination = '';
+        }
+    }
 }
